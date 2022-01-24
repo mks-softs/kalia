32d3d35eca9669c0d5762f9f4905faec941a0ede
siblonet <70165373+siblonet@users.noreply.github.com>
2022-01-24 12:42:58 +0000

    all

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = function kalya(string) {
-    if (typeof string !== "string") throw new TypeError("Kalya wants a string!");
+module.exports = function kalia(string) {
+    if (typeof string !== "string") throw new TypeError("kalia wants a string!");
     return string.replace(/\s/g, "");
   };
   
